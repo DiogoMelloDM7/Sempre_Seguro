657dcdcb848d4175c9f32db2eb64ad288c52e354
Diogo Mello <diogomellodm7@gmail.com>
2023-07-30 15:48:57 -0300

    Ajustando pagina de erros

--- sempre_seguro/settings.py
+++ sempre_seguro/settings.py
@@ -82,8 +82,8 @@ WSGI_APPLICATION = 'sempre_seguro.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-'''
-DATABASES = {
+
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
